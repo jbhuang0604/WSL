4727edea27712989af22b347dae0fdec3119e622
Dong Li <lidonggg930@gmail.com>
2016-05-09 14:48:53 -0700

    Update pascal_voc.py

--- lib/datasets/pascal_voc.py
+++ lib/datasets/pascal_voc.py
@@ -1,9 +1,13 @@
-# --------------------------------------------------------
-# Fast R-CNN
-# Copyright (c) 2015 Microsoft
-# Licensed under The MIT License [see LICENSE for details]
-# Written by Ross Girshick
-# --------------------------------------------------------
+# AUTORIGHTS
+# ---------------------------------------------------------
+# Copyright (c) 2015, Microsoft
+# Copyright (c) 2016, Dong Li
+# 
+# This file is part of the WSL code and is available 
+# under the terms of the MIT License provided in 
+# LICENSE. Please retain this notice and LICENSE if you use 
+# this file (or any portion of it) in your project.
+# ---------------------------------------------------------
 
 import datasets
 import datasets.pascal_voc
