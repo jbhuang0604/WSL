67e6389befb3da797968252424c1551e53a50133
Dong Li <lidonggg930@gmail.com>
2016-05-09 14:49:41 -0700

    Update imdb.py

--- lib/datasets/imdb.py
+++ lib/datasets/imdb.py
@@ -1,9 +1,13 @@
-# --------------------------------------------------------
-# Fast R-CNN
-# Copyright (c) 2015 Microsoft
-# Licensed under The MIT License [see LICENSE for details]
-# Written by Ross Girshick
-# --------------------------------------------------------
+# AUTORIGHTS
+# ---------------------------------------------------------
+# Copyright (c) 2014, Microsoft
+# Copyright (c) 2016, Dong Li
+# 
+# This file is part of the WSL code and is available 
+# under the terms of the MIT License provided in 
+# LICENSE. Please retain this notice and LICENSE if you use 
+# this file (or any portion of it) in your project.
+# ---------------------------------------------------------
 
 import os
 import os.path as osp
