b247f40450d814310f2af693aba0a5d44acaae0d
Dong Li <lidonggg930@gmail.com>
2016-05-09 14:54:51 -0700

    Update mil_classes_20x1_smooth_lsvm_topK_bagmine_greedycover.m

--- mil/mil_classes_20x1_smooth_lsvm_topK_bagmine_greedycover.m
+++ mil/mil_classes_20x1_smooth_lsvm_topK_bagmine_greedycover.m
@@ -1,19 +1,16 @@
-function mil_classes_20x1_smooth_lsvm_topK_bagmine_greedycover(classid)
-                  
-% HOS: find the highest latent_iter and only evaluate mAP on it.       
-%       the ideas is since I only want the maximum mAP, its's quicker to
-%         go back and evaluate mAP on it's precursors.
-       
 % AUTORIGHTS
 % ---------------------------------------------------------
 % Copyright (c) 2014, Hyun Oh Song
+% Copyright (c) 2016, Dong Li
 % 
-% This file is part of the Song-ICML2014 code and is available 
-% under the terms of the Simplified BSD License provided in 
+% This file is part of the WSL code and is available 
+% under the terms of the MIT License provided in 
 % LICENSE. Please retain this notice and LICENSE if you use 
 % this file (or any portion of it) in your project.
 % ---------------------------------------------------------
-       
+
+function mil_classes_20x1_smooth_lsvm_topK_bagmine_greedycover(classid)
+                  
 if ischar(classid),     classid = str2double(classid); end
 
 conf = voc_config();
