5c2ae5a3062e640f5b30c51feedf6c42e443b956
Dong Li <lidonggg930@gmail.com>
2016-05-09 14:39:47 -0700

    Update cache_fc8_features.m

--- features/cache_fc8_features.m
+++ features/cache_fc8_features.m
@@ -1,3 +1,14 @@
+% AUTORIGHTS
+% ---------------------------------------------------------
+% Copyright (c) 2014, Ross Girshick
+% Copyright (c) 2016, Dong Li
+% 
+% This file is part of the WSL code and is available 
+% under the terms of the MIT License provided in 
+% LICENSE. Please retain this notice and LICENSE if you use 
+% this file (or any portion of it) in your project.
+% ---------------------------------------------------------
+
 function cache_fc8_features(imdb, varargin)
 
 ip = inputParser;
