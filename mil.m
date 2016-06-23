8758767a2797a1036126dabb8c458e777802d720
Dong Li <lidonggg930@gmail.com>
2016-06-23 14:42:02 +0800

    Bug fixing
    cache_fc7_feature

--- mil.m
+++ mil.m
@@ -13,17 +13,21 @@ voc_init;
 classes = VOCopts.classes;
 
 % save image id for each class
-for cls = 1:length(classes)
-    [ids,flag] = textread(sprintf(VOCopts.clsimgsetpath,classes{cls},'trainval'),'%s %d');
-    count = 0;
-    for i = 1:length(ids)
-        if flag(i) ~= -1
-            count = count + 1;
-            class_pos_images(cls).ids(count) = ids(i);
+try
+    load('./mil/class_pos_images.mat');
+catch
+    for cls = 1:length(classes)
+        [ids,flag] = textread(sprintf(VOCopts.clsimgsetpath,classes{cls},'trainval'),'%s %d');
+        count = 0;
+        for i = 1:length(ids)
+            if flag(i) ~= -1
+                count = count + 1;
+                class_pos_images(cls).ids(count) = ids(i);
+            end
         end
     end
+    save('./mil/class_pos_images.mat','class_pos_images');
 end
-save('./mil/class_pos_images.mat','class_pos_images');
 
 % extract fc7 features
 net_file = 'caffenet_cls_adapt_iter_10000.caffemodel';
@@ -31,23 +35,13 @@ crop_mode = 'warp';
 crop_padding = 16;
 VOCdevkit = './data/VOCdevkit2007';
 imdb_train = imdb_from_voc(VOCdevkit, 'trainval', '2007');
-% using the selected proposals for the train set
 cache_fc7_features(imdb_train, ...
     'crop_mode', crop_mode, ...
     'crop_padding', crop_padding, ...
     'net_file', net_file, ...
-    'cache_name', 'voc_2007_trainval_fc7', ...
-    'select', 1);
-imdb_test  = imdb_from_voc(VOCdevkit, 'test', '2007');
-% using all the proposals for the test set
-cache_fc7_features(imdb_test, ...
-    'crop_mode', crop_mode, ...
-    'crop_padding', crop_padding, ...
-    'net_file', net_file, ...
-    'cache_name', 'voc_2007_test_fc7', ...
-    'select', 0);
+    'cache_name', 'voc_2007_trainval_fc7');
 
-% use mil to mine confident regions
+% empoly mil to mine confident regions
 tmp = pwd;
 cd('mil');
 for cls = 1:length(class_pos_images)
